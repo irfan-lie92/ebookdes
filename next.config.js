5bbfa85a06eed6828a2163f188f14b6784ff2b53
irfanali92 <ir.vanaly@gmail.com>
2025-07-21 11:45:48 +0700

    Update konfigurasi lokal pada next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com', 'cdn.jsdelivr.net', 'avatars.githubusercontent.com'], // tambah domain image jika perlu
-  },
-  reactStrictMode: true,
-  swcMinify: true, // gunakan compiler SWC untuk optimasi build
-  eslint: {
-    ignoreDuringBuilds: true, // abaikan error eslint saat build di Vercel
-  },
-  typescript: {
-    ignoreBuildErrors: true, // opsional: abaikan error TS saat build jika belum strict
-  },
-  experimental: {
-    // Hanya gunakan jika butuh fitur experimental, kalau tidak bisa dihapus
-  },
+    images: {
+        domains: ['images.unsplash.com'],
+    }
 };
+
+if (process.env.NEXT_PUBLIC_TEMPO) {
+    nextConfig["experimental"] = {
+        // NextJS 13.4.8 up to 14.1.3:
+        // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
+        // NextJS 14.1.3 to 14.2.11:
+        swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
+
+        // NextJS 15+ (Not yet supported, coming soon)
+    }
+}
 
 module.exports = nextConfig;
